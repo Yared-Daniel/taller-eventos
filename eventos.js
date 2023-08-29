d6e07f92612f45fc68818e8ad129c08f693c03cf
Yared-Daniel <141077128+Yared-Daniel@users.noreply.github.com>
2023-08-29 00:06:50 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,11 +1,13 @@
-//function mostrarAlerta() {
-    alert("¡Hola!");
-//}
-
 // Obtener una referencia al div mediante su ID
-const divElement = document.getElementById("miDiv");
+const divElement = document.getElementById("miDiv"); //Para el div
+const bottonSaludo = document.getElementById("btnSaludo") // directamente al botton Saludar
 
 // Agregar un manejador de eventos al clic en el div
-divElement.addEventListener("click", function() {
+/*divElement.addEventListener("click", function() {   
   alert("Hola! Soy el div");
-});
+}); */
+
+bottonSaludo.addEventListener("click", function() {   
+    alert("Hola! Soy el div");
+  });
+  
