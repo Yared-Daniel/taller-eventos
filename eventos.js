62b148042a62427c0a31d9683be1e0901d214426
Yared-Daniel <141077128+Yared-Daniel@users.noreply.github.com>
2023-08-28 23:33:30 -0300

    Commit3 - intento de añadir JS

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
 //function mostrarAlerta() {
     alert("¡Hola!");
 //}
+
+// Obtener una referencia al div mediante su ID
+const divElement = document.getElementById("miDiv");
+
+// Agregar un manejador de eventos al clic en el div
+divElement.addEventListener("click", function() {
+  alert("Hola! Soy el div");
+});
